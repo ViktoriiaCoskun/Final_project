5816849b7d3f55b5117e686fd0c366066dd52029
Viktoriia Coskun <vicky.9995022@gmail.com>
2023-03-28 02:39:39 +0200

    blog.py

--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,54 @@
-# blog/routes.py
+from flask import Flask,render_template,request
 
-from flask import render_template
-from blog import app
-
+from blog.models import Entry,db
+from blog.forms import EntryForm
+from faker import Faker
+app = Flask(__name__)
 @app.route("/")
 def index():
-   return render_template("base.html")
+   
+   all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+   return render_template("homepage.html", all_posts=all_posts)
+
+@app.route("/new_post/", methods=["GET", "POST"])
+def create_entry():
+   form = EntryForm()
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry()
+           entry.title=form.title.data
+           entry.body=form.body.data
+           entry.is_published=form.is_published.data
+               
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   form = EntryForm(obj=entry)
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           form.populate_obj(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
+
+def generate_entries(how_many=10):
+   fake = Faker()
+
+   for i in range(how_many):
+      post = Entry(
+            title=fake.sentence(),
+            body='\n'.join(fake.paragraphs(15)),
+            is_published=True
+      )
+      db.session.add(post)
+   db.session.commit()
+
--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
    SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"   
    SQLALCHEMY_DATABASE_URI = (                           
            os.environ.get('DATABASE_URL') or
-           'sqlite:///' + os.path.join(BASE_DIR, 'booklibrary.db')
+           'sqlite:///' + os.path.join(BASE_DIR, 'db.db')
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
